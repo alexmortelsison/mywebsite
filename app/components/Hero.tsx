bc66bfbb48e553b92d7b9bc9579b75986a09fead
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-25 08:16:30 +0900

    fixed padding

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function HeroPage() {
   return (
-    <div className="z-10 lg:px-16 2xl:px-0 tracking-tighter ">
+    <div className="z-10 tracking-tighter ">
       <div className="">
         <div className="">
           <Image
@@ -12,14 +12,14 @@ export default function HeroPage() {
             className="object-cover -z-50 brightness-50"
           />
           <div className="bg-gray-800/20 backdrop-blur-sm h-[90vh]">
-            <div className="max-w-7xl mx-auto z-10 px-8">
-              <h1 className="pt-[250px] 2xl:pt-[350px] lg:pt-[300px] md:pt-[500px] text-white font-bold text-4xl lg:text-9xl">
+            <div className="max-w-7xl mx-auto z-10 px-8 lg:px-16 2xl:px-6">
+              <h1 className="pt-[250px] 2xl:pt-[400px] lg:pt-[300px] md:pt-[500px] text-white font-normal text-4xl lg:text-6xl">
                 Alex Sison.
               </h1>
-              <p className="text-white font-normal text-3xl lg:text-6xl">
+              <p className="text-white font-bold text-3xl lg:text-6xl">
                 Full Stack Developer.
               </p>
-              <p className="text-gray-400 text-md lg:text-xl lg:w-1/3 pt-2">
+              <p className="text-gray-400 text-md lg:text-lg lg:w-1/3 pt-2">
                 A dedicated web developer with a strong passion for creating
                 efficient, high-performing, and user-friendly web applications.
               </p>
