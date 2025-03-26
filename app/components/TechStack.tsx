aea22afa540ceceed4d349980ee03ba6542f739b
alexmortelsison <150509106+alexmortelsison@users.noreply.github.com>
2025-03-27 08:18:23 +0900

    updated techstack

--- app/components/TechStack.tsx
+++ app/components/TechStack.tsx
@@ -2,11 +2,10 @@ import { TbBrandTypescript } from "react-icons/tb";
 import { SiTailwindcss } from "react-icons/si";
 import { DiReact } from "react-icons/di";
 import { TbBrandNextjs } from "react-icons/tb";
-import Image from "next/image";
 export default function TechStack() {
   return (
     <div className="lg:h-[50vh] md:h-[25vh] bg-white z-50 tracking-tighter mt-24 md:mt-4">
-      <div className="max-w-7xl mx-auto flex flex-col justify-center items-center pt-8 md:pt-0 2xl:pt-80 lg:pt-48">
+      <div className="flex flex-col justify-center items-center pt-8 md:pt-0 2xl:pt-80 lg:pt-48">
         <h1 className="lg:text-xl">Main Tech Stack</h1>
         <p className="text-center text-2xl lg:text-4xl px-8 lg:px-0 pt-2 lg:pt-4 lg:max-w-[800px]">
           I use Next.Js, React, TypeScript, and TailwindCSS for fast and
@@ -15,16 +14,17 @@ export default function TechStack() {
 
         <div className="relative flex justify-center items-center">
           <div>
-            <Image
-              src={"/smoke.png"}
-              alt="smoke"
+            <video
+              src={"/stack.mp4"}
               width={1000}
               height={1000}
-              className="object-cover w-screen"
+              loop
+              autoPlay
+              className="object-cover"
             />
           </div>
           <div className="absolute -top-[1px] md:grid-cols-2 lg:grid-cols-4 max-w-7xl md:px-2 mx-auto gap-4 md:mt-12 lg:mt-24 z-40 hidden md:inline-grid">
-            <div className="flex flex-col rounded-4xl lg:h-[300px] lg:w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl">
+            <div className="flex flex-col rounded-4xl lg:h-[300px] lg:w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl hover:scale-110 duration-500">
               <div className="">
                 <TbBrandNextjs
                   size={200}
@@ -32,7 +32,7 @@ export default function TechStack() {
                 />
               </div>
             </div>
-            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl">
+            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl hover:scale-110 duration-500">
               <div className="">
                 <DiReact
                   size={200}
@@ -40,7 +40,7 @@ export default function TechStack() {
                 />
               </div>
             </div>
-            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl">
+            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl hover:scale-110 duration-500">
               <div className="">
                 <TbBrandTypescript
                   size={200}
@@ -48,7 +48,7 @@ export default function TechStack() {
                 />
               </div>
             </div>
-            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl">
+            <div className="flex flex-col rounded-4xl lg:h-[300px] w-[200px] md:h-[200px] md:w-[200px] justify-center items-center bg-gray-600/20 backdrop-blur-xl hover:scale-110 duration-500">
               <div className="">
                 <SiTailwindcss
                   size={200}
